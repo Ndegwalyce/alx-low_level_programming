683e926a5f1afaadad29c4b29e25cd5298499f9a
Ndegwalyce <ndegwaalice13@gmail.comice>
2023-08-10 14:36:45 -0700

    c programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include<stdio.h>
+/**
+ * main - entry point
+ * Description: print a statement using puts function
+ * Return: (0) success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
