adbd8ae62abedbcbc02fcd2d6c42aeaee0186e05
Ndegwalyce <ndegwaalice13@gmail.comice>
2023-08-11 15:20:04 -0700

    c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0 on program success
+ */
+
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+	{
+		putchar(digit + '0');
+
+			if (digit != 9)
+				{
+				putchar(',');
+				putchar(' ');
+				}
+	}
+
+	putchar('\n');
+
+return (0);
+}
