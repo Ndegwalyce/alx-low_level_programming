5fe07c6d83f99c3efb389c2ad492c9647235da7b
Ndegwalyce <ndegwaalice13@gmail.comice>
2023-08-11 12:58:25 -0700

    variables

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always return 0 (Success)
+ */
+
+int main (void)
+{
+	int n;
+	int y;
+        if (y > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, y);
+	if (y == 0)
+		printf("Last digit of %d is %d and is 0\n", n, y);
+	if (y < 6 && y != 0)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, y);
+	return (0);
+}
