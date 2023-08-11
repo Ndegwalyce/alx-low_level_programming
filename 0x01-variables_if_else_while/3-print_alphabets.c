75e8ee1c10ddb5f7866adc0fe9bfbcdde8e53e00
Ndegwalyce <ndegwaalice13@gmail.comice>
2023-08-11 15:09:37 -0700

    c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,5 +1,3 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
 /**
@@ -7,7 +5,8 @@
  * Return: 0 on success of program
  */
 
-int main (void)
+int main(void)
+{
 	int digit;
 
 	for (digit = 0; digit < 10; digit++)
